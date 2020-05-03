92959553ded8c3474061194d327d84e667f67992
JuicyJerry <ju2cyjerry@gmail.com>
2020-05-03 18:48:21 +0900

    #1. 문자 입력받고 출력하기

---
+++ level6-1/level6-1/소스.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+char ch1, ch2;
+void ABC()
+{
+	cin >> ch1 >> ch2;
+}
+void OUTPUT()
+{
+	cout << ch1 << " " << ch2;
+}
+int main()
+{
+	ABC();
+	OUTPUT();
+
+	return 0;
+}
