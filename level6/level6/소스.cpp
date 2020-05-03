aa2481a902b50d7055b786bad3b86c433ce73538
JuicyJerry <ju2cyjerry@gmail.com>
2020-05-03 19:00:03 +0900

    #2. 아스키코드 문자번호를 밝혀라

---
+++ level6/level6/소스.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{	char a, b, c;
+cin >> a >> b >> c;
+
+cout << int(a) << endl;
+cout << (int)b << endl;
+cout << (int)(c) << endl;
+
+	return 0;
+}
