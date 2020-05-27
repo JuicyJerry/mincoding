aa068bdda5a39c8f8815a99808ca8534b2745e8c
JuicyJerry <ju2cyjerry@gmail.com>
2020-05-27 13:50:24 +0900

    민코딩 시작반 level9 문제 코드입니다

--- (시작반2) Level 9 - 문제6번.cpp
+++ (시작반2) Level 9 - 문제6번.cpp
@@ -1,1 +1,30 @@
-Runtime Error;
+#include <iostream>
+using namespace std;
+int main() {
+	char vect[3][3];
+	char alp = 'A';
+	int y0, y1, x0, x1;
+	char temp;
+
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 3; x++) {
+			vect[y][x] = alp;
+			alp++;
+		}
+	}
+
+	cin >> y0 >> y1 >> x0 >> x1;
+
+	temp = vect[y0][y1];
+	vect[y0][y1] = vect[x0][x1];
+	vect[x0][x1] = temp;
+
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 3; x++) {
+			cout << vect[y][x];
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
