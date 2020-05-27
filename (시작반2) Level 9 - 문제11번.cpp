60059b5a1f28a927101a2247a8ede3edb579c2a0
JuicyJerry <ju2cyjerry@gmail.com>
2020-05-27 10:45:46 +0900

    민코딩 시작반 level9 문제 코드입니다

--- (시작반2) Level 9 - 문제11번.cpp
+++ (시작반2) Level 9 - 문제11번.cpp
@@ -1,1 +1,24 @@
-ing;
+#include <iostream>
+using namespace std;
+struct Person {
+	int age, height;
+};
+
+Person a, b;
+
+void input() {
+	cin >> a.age >> a.height >> b.age >> b.height;
+
+}
+
+void output() {
+	cout << (a.age + b.age) / 2 << " ";
+	cout << (a.height + b.height) / 2;
+}
+
+int main() {
+	input();
+	output();
+
+	return 0;
+}
