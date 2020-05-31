2bc895206f8914d55eb96ad70adc0a3c39e5ba45
JuicyJerry <ju2cyjerry@gmail.com>
2020-05-31 17:46:14 +0900

    민코딩 시작반 level10.5 문제 코드입니다

--- (시작반2) Level10.5 - 문제4번.cpp
+++ (시작반2) Level10.5 - 문제4번.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
 using namespace std;
-int vect[3][5] = {
+char vect[3][5] = {
 	'D', 'A', 'C', 'C', 'D',
 	'S', 'D', 'F', 'A', 'E',
 	'E', 'E', 'T', 'J', 'H'
 };
 
-int INPUT(int value) {
+int CHECK(char value) {
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 5; x++) {
+			if (vect[y][x] == value) return 1;
+		}
+	}
+	return 0;
+}
+
+void INPUT() {
 	char chr;
 	cin >> chr;
-	int check = CHECK(chr);
-	cout << check;
-
-	if (value == 1) cout << "있음";
+	if (CHECK(chr) == 1) cout << "있음";
 	else cout << "없음";
 }
 
-void CHECK(char chr) {
-	for (int y = 0; y < 3; y++) {
-		for (int x = 0; x < 5; x++) {
-			if (vect[y][x] == chr) return INPUT(1);
-			else return INPUT(0);
-		}
-	}
-}
-
 int main() {
-	INPUT(0);
-
+	INPUT();
 
 	return 0;
 }
