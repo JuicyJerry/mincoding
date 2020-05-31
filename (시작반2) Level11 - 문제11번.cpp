374a344a5293d8212320f5a79bd0581dafb8df0b
JuicyJerry <ju2cyjerry@gmail.com>
2020-05-31 18:33:55 +0900

    (시작반2) Level11 - 문제11번

--- (시작반2) Level11 - 문제11번.cpp
+++ (시작반2) Level11 - 문제11번.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 using namespace std;
-int vect[4][4] = {
-	1, 3, 6, 2,
-	4, 2, 4, 5,
-	6, 3, 7, 3,
-	1, 5, 4, 6
-};
-
 int main() {
-	int input;
-	int select[16];
-	cin >> input;
+	int vect[4][4] = {
+		1, 3, 6, 2,
+		4, 2, 4, 5,
+		6, 3, 7, 3,
+		1, 5, 4, 6
+	};
+
+	int num;
+	cin >> num;
+	int select[16] = { 0 };
+	int t = 0;
+
 	for (int y = 0; y < 4; y++) {
 		for (int x = 0; x < 4; x++) {
-			if (vect[y][x] > input) select[16] = vect[y][x];
+			if (vect[y][x] > num) {
+				select[t] = vect[y][x];
+				t++;
+			}
 		}
 	}
 
-	for (int i = 0; i < 16; i++) cout << select[i];
+	for (int i = 0; i < t; i++) {
+		cout << select[i] << " ";
+	}
 
 	return 0;
 }
