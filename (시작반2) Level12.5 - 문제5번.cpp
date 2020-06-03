1bceea6497531349a5c1675c8f4c52d4ab4a9f69
JuicyJerry <ju2cyjerry@gmail.com>
2020-06-03 23:03:46 +0900

    민코딩 시작반 level12.5 문제 코드입니다

--- (시작반2) Level12.5 - 문제5번.cpp
+++ (시작반2) Level12.5 - 문제5번.cpp
@@ -9,17 +9,11 @@ int main() {
 		6, 7, 6, 7, 6
 	};
 
-	char a, b;
-	int c, d, e;
-	int f, g, h, i, j;
-	int n = 0;
-
-	cin >> a >> b;
-	for (int j = 0; j < 4; j++) {
-		cin >> vect[a + n][b + n];
-		vect[a + n][b + n]++;
-		if (vect[a][b] == 10) vect[a][b] = 0;
-		n++;
+	int x, y;
+	for (int i = 0; i < 5; i++) {
+		cin >> y >> x;
+		vect[y][x]++;
+		vect[y][x] = vect[y][x] % 10;
 	}
 
 	for (int y = 0; y < 5; y++) {
