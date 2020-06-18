8184bc5f4415e9a37b3e91e9c01ef27d43a1a8a3
JuicyJerry <ju2cyjerry@gmail.com>
2020-06-18 16:01:40 +0900

    민코딩 시작반 level14.5 문제 코드입니다

--- (시작반2) Level14.5 - 문제4번.cpp
+++ (시작반2) Level14.5 - 문제4번.cpp
@@ -3,44 +3,20 @@ using namespace std;
 struct Product
 {
 	int strawberry, melon, watermelon;
-	int A[3] = { strawberry = 300, melon = 500, watermelon = 1000 };
-	int B[3] = { strawberry = 450, melon = 450, watermelon = 900 };
-	int C[3] = { strawberry = 200, melon = 150, watermelon = 700 };
 };
 
 
 int main() {
-	Product A;
-	Product B;
-	Product C;
+	Product A = { 300, 500, 1000 };
+	Product B = { 450, 450, 900 };
+	Product C = { 200, 150, 700 };
 
 	char mart;
 	cin >> mart;
-	if (mart == 'A') {
-		int sum1 = 0;
-		for (int i = 0; i < 3; i++) {
-			sum1 += A.A[i];
-		}
-		cout << sum1 / 3;
-	}
 
-	if (mart == 'B') {
-		int sum2 = 0;
-		for (int i = 0; i < 3; i++) {
-			sum2 += B.A[i];
-		}
-		cout << sum2 / 3;
-	}
-
-	if (mart == 'C') {
-		int sum3 = 0;
-		for (int i = 0; i < 3; i++) {
-			sum3 += C.A[i];
-		}
-		cout << sum3 / 3;
-	}
-
-
+	if (mart == 'A') cout << (A.strawberry + A.melon + A.watermelon) / 3;
+	if (mart == 'B') cout << (B.strawberry + B.melon + B.watermelon) / 3;
+	if (mart == 'C') cout << (C.strawberry + C.melon + C.watermelon) / 3;
 
 	return 0;
 }
