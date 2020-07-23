3ce50b21ecec17be1ca13ee94cd9de146fa09539
JuicyJerry <ju2cyjerry@gmail.com>
2020-07-23 16:01:26 +0900

    민코딩 훈련반 level23 문제 코드입니다

--- (훈련반1) Level23 - 문제1번.cpp
+++ (훈련반1) Level23 - 문제1번.cpp
@@ -5,6 +5,9 @@ char path[4];
 
 void run(int level)
 {
+	if (level >= 2 && path[level - 2] == path[level - 1]) return;
+	if (level >= 3 && path[level - 3] == path[level - 1]) return;
+
 	if (level == 3)
 	{
 		cout << path << endl;
@@ -13,16 +16,15 @@ void run(int level)
 
 	for (int i = 0; i < 4; i++) {
 		path[level] = name[i];
-		if (level >= 1 && path[level] == path[level - 1] || path[level] == path[level - 2]) continue;
 		run(level + 1);
 		path[level] = 0;
 	}
+
 }
 
 int main()
 {
 	cin >> name;
 	run(0);
-
 	return 0;
 }
