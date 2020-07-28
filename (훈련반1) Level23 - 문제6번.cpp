430299efd14d4a930cc7b2a679b316e29fee8a52
JuicyJerry <ju2cyjerry@gmail.com>
2020-07-28 22:34:09 +0900

    민코딩 훈련반 level23 문제 코드입니다

--- (훈련반1) Level23 - 문제6번.cpp
+++ (훈련반1) Level23 - 문제6번.cpp
@@ -1,32 +1,44 @@
 #include <iostream>
 using namespace std;
+char vect[6];
 char path[10];
-char card[6];
-int cnt;
+int cnt = 0;
+
+
+int abs(int x)
+{
+	if (x < 0) return -x;
+	return x;
+}
+
 void run(int level)
 {
-	if (level >= 1 && path[level] - path[level - 1] > 3) return;
-	// if (level >= 1 && path[level - 1] - path[level] > 3) return;
-	// if (level >= 1 && path[level - 2] - path[level - 1] > 3) return;
+	if (level >= 2 && (path[level - 1]) - (path[level - 2]) > 3) return;
 
 	if (level == 4)
 	{
 		cnt++;
-		cout << path << endl;
+		//cout << path << endl;
 		return;
 	}
+
 	for (int i = 0; i < 5; i++)
 	{
-		path[level] = card[i];
-		run(level + 1);
-		path[level] = 0;
+		if (!(level > 0 && abs(path[level - 1] - vect[i]) > 3))
+		{
+			path[level] = vect[i];
+			run(level + 1);
+			path[level] = 0;
+		}
 	}
+
 }
 
 int main()
 {
-	cin >> card;
+	cin >> vect;
 	run(0);
 	cout << cnt;
+
 	return 0;
 }
