93129a3abc4b71009578bcf399029b43d2ede112
JuicyJerry <ju2cyjerry@gmail.com>
2020-07-29 21:15:32 +0900

    민코딩 훈련반 level22 문제 코드입니다

--- (훈련반1) Level22 - 문제4번.cpp
+++ (훈련반1) Level22 - 문제4번.cpp
@@ -1,36 +1,61 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
+int path[12];
+int name[11] = { -5, -4, -3, -2, -1, 0, 2, 3, 4, 5, 6 };
+char input[5];
+int ret1, i;
+int t = 1;
+int x = 5;
 
-char floorNum[11] = "432112345";
-char upDownCin[5];
-char up[3] = "up";
-char down[5] = "down";
-int t = 4;
-int len;
+void run(int level)
+{
+	if (level == 1)
+	{
+		//cout << path[0] << endl;
+		return;
+	}
+	else if (level == -1)
+	{
+		//cout << path[0] << endl;
+		return;
+	}
+
+	if (ret1 == 0)
+	{
+		path[level] = name[x += t];
+		run(level + 1);
+		if (i == 4)
+		{
+			if (path[0] == 0) cout << 1;
+			else if (path[0] < 0) cout << 'B' << (-path[0]);
+			else cout << path[0];
+		}
+		path[level] = 0;
+	}
+	else if (ret1 != 0)
+	{
+		path[level] = name[x -= t];
+		run(level - 1);
+		if (i == 4)
+		{
+			if (path[0] == 0) cout << 1;
+			else if (path[0] < 0) cout << 'B' << (-path[0]);
+			else cout << path[0];
+		}
+		path[level] = 0;
+	}
+}
 
 int main()
 {
-	for (int i = 0; i < 5; i++) {
-		cin >> upDownCin;
+	for (i = 0; i < 5; i++)
+	{
+		cin >> input;
 
-		for (int i = 0; i < 5; i++) {
-			if (upDownCin[i] == '\0') {
-				len = i;
-				break;
-			}
-		}
-		for (int i = 0; i <= len; i++) {
-			if (upDownCin[i] == up[i] && upDownCin[i] == '\0') {
-				t++;
-			}
-			else if (upDownCin[i] == down[i] && upDownCin[i] == '\0') {
-				t--;
-			}
-		}
+		ret1 = strcmp(input, "up");
+		run(0);
 	}
-
-	cout << floorNum[t];
 
 	return 0;
 }
