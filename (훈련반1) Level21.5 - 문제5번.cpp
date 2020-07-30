224e928d768934e6f79a8026ad10c5c218a6815a
JuicyJerry <ju2cyjerry@gmail.com>
2020-07-30 16:40:48 +0900

    민코딩 훈련반 level21.5 문제 코드입니다

--- (훈련반1) Level21.5 - 문제5번.cpp
+++ (훈련반1) Level21.5 - 문제5번.cpp
@@ -3,23 +3,25 @@ using namespace std;
 int main()
 {
 	int vect[8];
-	for (int i = 0; i < 8; i++) {
-		cin >> vect[i];
-	}
 	int bucket[10] = { 0 };
-	for (int j = 0; j < 10; j++) {
-		int cnt = 0;
 
-		for (int i = 0; i < 10; i++) {
-			if (vect[j] == i) {
-				cnt++;
-				bucket[j] = cnt;
+	for (int i = 0; i < 8; i++)
+	{
+		cin >> vect[i];
+
+		bucket[vect[i]]++;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (bucket[i] == 0) continue;
+		if (bucket[i] > 0)
+		{
+			for (int j = 0; j < bucket[i]; j++)
+			{
+				cout << i << " ";
 			}
 		}
-	}
-
-	for (int i = 0; i < 10; i++) {
-		cout << bucket[i] << " ";
 	}
 
 	return 0;
--- (훈련반1) Level21.5 - 문제7번.cpp
+++ (훈련반1) Level21.5 - 문제7번.cpp
@@ -1,38 +1,57 @@
 #include <iostream>
 using namespace std;
+
+int direct[4][2] =
+{
+	-1, 0,
+	1, 0,
+	0, 1,
+	0, -1
+};
+
+// 0: white, 1: black, #: nothing
+char map[7][8] =
+{
+	"#######",
+	"##0#0##",
+	"#01#10#",
+	"##010##",
+	"##10#0#",
+	"#00####",
+	"#######"
+};
+
 int main()
 {
-	// empty : 0, white : 1, black: 2
-	int map[7][7] =
-	{
-		0, 0, 1, 0, 1, 0, 0,
-		0, 1, 2, 0, 2, 1, 0,
-		0, 0, 1, 2, 1, 0, 0,
-		0, 0, 2, 1, 0, 1, 0,
-		0, 1, 1, 0, 0, 0, 0,
-		0, 1, 1, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0
-	};
-
-	int direct[4][2] =
-	{
-		-1, 0,
-		1, 0,
-		0, -1,
-		0, 1
-	};
 	int dy, dx;
+	int y, x;
+	cin >> y >> x;
+	map[y][x] = '0';
+	int realCnt = 0;
 
-
-	for (int j = 0; j < 7; j++) {
-		for (int i = 0; i < 7; i++) {
-			for (int t = 0; t < 4; t++) {
+	for (int y = 0; y < 7; y++)
+	{
+		for (int x = 0; x < 7; x++)
+		{
+			int cnt = 0;
+			for (int t = 0; t < 4; t++)
+			{
 				dy = y + direct[t][0];
 				dx = x + direct[t][1];
-				if (map[dy][dx] == 1) cnt++;
+
+				if (dy < 0 || dx < 0 || dx > 6 || dy > 6) continue;
+				else if (map[dy][dx] != '0') break;
+				else cnt++;
+
+				if (cnt == 4)
+				{
+					cnt = 0;
+					realCnt++;
+				}
 			}
 		}
 	}
 
+	cout << realCnt;
 	return 0;
 }
