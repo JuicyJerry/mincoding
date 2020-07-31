ed5fa166e65df1ab23fb9dab0e7e3e922cf6d504
JuicyJerry <ju2cyjerry@gmail.com>
2020-07-31 17:02:36 +0900

    민코딩 훈련반 level21 문제 코드입니다

--- (훈련반1) Level21 - 문제2번.cpp
+++ (훈련반1) Level21 - 문제2번.cpp
@@ -1,1 +1,14 @@
-? ? ?
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+	char id[100];
+	char password[100];
+	cin >> id >> password;
+
+	if (strcmp(id, "qlqlaqkq") == 0 && strcmp(password, "tkaruqtkf") == 0) cout << "LOGIN";
+	else cout << "INVALID";
+
+	return 0;
+}
