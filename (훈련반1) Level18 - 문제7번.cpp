eac50e6ab3bb11fa6bb63086bd106f1c037be1c3
JuicyJerry <ju2cyjerry@gmail.com>
2020-08-07 16:06:27 +0900

    민코딩 훈련반 level18 문제 코드입니다

--- (훈련반1) Level18 - 문제7번.cpp
+++ (훈련반1) Level18 - 문제7번.cpp
@@ -1,37 +1,33 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
-int main() {
-	int train[8] = { 3, 7, 6, 4, 2, 9, 1, 7 };
-	int team[3];
-	for (int i = 0; i < 3; i++) {
-		cin >> team[i];
+
+int train[8] = { 3, 7, 6, 4, 2, 9, 1, 7 };
+int team[3];
+
+int whereIsMyTeam(int y)
+{
+	for (int t = 0; t < 8; t++)
+	{
+		if (train[t] == team[y]) return t;
+	}
+	return 0;
+}
+
+int main()
+{
+	for (int y = 0; y < 3; y++)
+	{
+		cin >> team[y];
 	}
 
-	int dat[10] = { 0 };
-	for (int i = 0; i < 8; i++) {
-		dat[train[i]]++;
-	}
-	int t = 0;
-	int n = 0;
-	int temp[3] = { 0 };
-	for (int j = 0; j < 3; j++) {
-		for (int i = 0; i < 10; i++) {
-			if (dat[i] >= 1) {
-				n = i;
-				for (int z = 0; z < 9; z++) {
-					if (n == train[z]) {
-						temp[t] = n;
-						t++;
-					}
-				}
+	for (int y = 0; y < 3; y++)
+	{
+		int ret = whereIsMyTeam(y);
+		if (y == 0 && ret > 0) cout << ret << "번~";
+		if (y == 2 && ret > 0) cout << ret << "번 칸";
 
-			}
-		}
 	}
 
-	cout << temp[0] << "~" << temp[2] << " ĭ";
 
 	return 0;
-
 }
