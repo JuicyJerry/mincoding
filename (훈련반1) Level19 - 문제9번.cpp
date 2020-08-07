a977b5ce6380ce5632d6dc473c8c7d41ef15dd27
JuicyJerry <ju2cyjerry@gmail.com>
2020-08-07 16:06:36 +0900

    민코딩 훈련반 level19 문제 코드입니다

--- (훈련반1) Level19 - 문제9번.cpp
+++ (훈련반1) Level19 - 문제9번.cpp
@@ -1,1 +1,46 @@
-? ? ?
+#include <iostream>
+using namespace std;
+int image[4][4];
+
+int rectSum(int j, int i)
+{
+	int sum = 0;
+	for (int y = j; y < j + 2; y++)
+	{
+		for (int x = i; x < j + 3; x++)
+		{
+			sum += image[y][x];
+		}
+	}
+	return sum;
+}
+
+int main()
+{
+	for (int y = 0; y < 4; y++)
+	{
+		for (int x = 0; x < 4; x++)
+		{
+			cin >> image[y][x];
+		}
+	}
+
+	int max = -999;
+	int yMaxIndex, xMaxIndex;
+	for (int y = 0; y < 3; y++)
+	{
+		for (int x = 0; x < 2; x++)
+		{
+			int ret = rectSum(y, x);
+			if (ret > max)
+			{
+				max = ret;
+				yMaxIndex = y;
+				xMaxIndex = x;
+			}
+		}
+	}
+
+	cout << "(" << yMaxIndex << "," << xMaxIndex << ")";
+	return 0;
+}
