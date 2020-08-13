9997b52b1de22b76b75463af1fbe13750dd6a103
JuicyJerry <ju2cyjerry@gmail.com>
2020-08-13 17:14:51 +0900

    민코딩 훈련반 level21 문제 코드입니다

--- (훈련반1) Level21 - 문제5번.cpp
+++ (훈련반1) Level21 - 문제5번.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
 #include <cstring>
-#include <string.h>
-#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
 using namespace std;
 int main()
 {
-	char vect[3][11];
-	int max = -999;
-	int maxIndex;
-
-	cin >> vect[0];
-	cin >> vect[1];
-	cin >> vect[2];
-
-
-	for (int i = 0; i < 3; i++)
+	char vect[3][10];
+	int firstVectIndex = 0;
+	for (int y = 0; y < 3; y++)
 	{
-		int len = strlen(vect[i]);
-		if (len > max)
-		{
-			max = len;
-			maxIndex = i;
-		}
+		cin >> vect[y];
 	}
 
-	char temp[11];
-	strcpy_s(temp, vect[maxIndex]);
-	strcpy_s(vect[maxIndex], vect[0]);
-	strcpy_s(vect[0], temp);
+	int len1 = strlen(vect[0]);
+	int len2 = strlen(vect[1]);
+	int len3 = strlen(vect[2]);
+
+	char temp[10];
+	if (len2 > len1 && len2 > len3)
+	{
+		strcpy_s(temp, vect[1]);
+		strcpy_s(vect[1], vect[0]);
+		strcpy_s(vect[0], temp);
+	}
+	else if (len3 > len2 && len3 > len1)
+	{
+		strcpy_s(temp, vect[3]);
+		strcpy_s(vect[3], vect[0]);
+		strcpy_s(vect[0], temp);
+	}
 
 	for (int y = 0; y < 3; y++)
 	{
-		cout << vect[y] << endl;
+		cout << vect[y];
+		cout << endl;
 	}
 
 	return 0;
