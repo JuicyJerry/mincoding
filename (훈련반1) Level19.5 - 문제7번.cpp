4fdc9133c40c2039d9d99bcafc4850aa6467f067
JuicyJerry <ju2cyjerry@gmail.com>
2020-08-13 17:14:30 +0900

    민코딩 훈련반 level19.5 문제 코드입니다

--- (훈련반1) Level19.5 - 문제7번.cpp
+++ (훈련반1) Level19.5 - 문제7번.cpp
@@ -1,48 +1,53 @@
 #include <iostream>
 using namespace std;
-int map[3][3] =
-{
-	3, 5, 1,
-	3, 8, 1,
-	1, 1, 5
-};
 
-int bitArray[2][2];
-int sum = 0;
-int max = 0;
-int maxYindex, maxXindex;
-int isPattern(int dy, int dx)
+int map[3][3] = { 3, 5, 1, 3, 8, 1, 1, 1, 5 };
+int bitarray[2][2];
+int sum = 0, m0, m1;
+
+void masking(int* j, int* i)
 {
-	for (int y = 0; y < 2; y++) {
-		for (int x = 0; x < 2; x++) {
-			if (bitArray[y][x] == map[y + dy][x + dx]) {
-				sum += map[y][x];
+	sum = 0;
+	for (int y = 0; y < 2; y++)
+	{
+		for (int x = 0; x < 2; x++)
+		{
+			if (bitarray[y][x] == 1)
+			{
+				sum += map[*j + y][*i + x];
 			}
+			else return;
 		}
 	}
-	return sum;
-
 }
 
-int main() {
 
-	for (int y = 0; y < 2; y++) {
-		for (int x = 0; x < 2; x++) {
-			cin >> bitArray[y][x];
+int main()
+{
+	for (int y = 0; y < 2; y++)
+	{
+		for (int x = 0; x < 2; x++)
+		{
+			cin >> bitarray[y][x];
 		}
 	}
 
-	for (int y = 0; y < 3; y++) {
-		for (int x = 0; x < 3; x++) {
-			int sum = isPattern(y, x);
-			if (sum > max) {
-				max = sum;
-				maxYindex = y;
-				maxXindex = x;
+	int maxx = 0;
+	for (int y = 0; y < 2; y++)
+	{
+		for (int x = 0; x < 2; x++)
+		{
+			masking(&y, &x);
+			if (sum > maxx)
+			{
+				maxx = sum;
+				m0 = y;
+				m1 = x;
 			}
 		}
 	}
-	cout << "(" << maxYindex << "," << maxXindex << ")";
+
+	cout << "(" << m0 << "," << m1 << ")";
 
 	return 0;
 }
