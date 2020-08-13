e50828b4555d7d2ceeb4fe13d4a638b36e2b9f86
JuicyJerry <ju2cyjerry@gmail.com>
2020-08-13 17:14:40 +0900

    민코딩 훈련반 level20.5 문제 코드입니다

--- (훈련반1) Level20.5 - 문제5번.cpp
+++ (훈련반1) Level20.5 - 문제5번.cpp
@@ -1,1 +1,23 @@
-? ? ?
+? ? ? #include <iostream>
+using namespace std;
+int main()
+{
+	char chr;
+	cin >> chr;
+	for (char t = chr - 3; t <= chr + 3; t++)
+	{
+		// if (t >= 83 && t < 90)
+		if (t < 65)
+		{
+			cout << (char)(t + 26);
+		}
+		//	 else if (t >= 65 && t <= 71)
+		else if (t > 90)
+		{
+			cout << (char)(t - 26);
+		}
+		else cout << t;
+	}
+
+	return 0;
+}
